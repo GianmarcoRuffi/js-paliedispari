7edf5abc8f0b44ad83f69efd993a4e2bc4ba2dd8
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-04-03 18:28:58 +0200

    progress esercizio 2

--- pariedispari.js
+++ pariedispari.js
@@ -6,15 +6,20 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.*/
 
 // Autoroll function for pc
-function autoRoll(rollNum) {
-  const maxNum = 5;
-  let rollNum = Math.floor(Math.random() * maxNum) + 1;
 
-  console.log("Rolled number: " + rollNum);
+function autoRoll() {
+  let resultNumber = Math.floor(Math.random() * 5) + 1;
+  return resultNumber;
 }
 
-autoRoll(cpuRoll);
-console.log("Rolled number: " + cpuRoll);
+//Total user + cpu
+function sum(num1, num2) {
+  let total = num1 + num2;
+  if (total % 2 === 0) {
+    return true;
+  }
+  return false;
+}
 
 // User choice
 
@@ -26,17 +31,31 @@ if (userChoice === "pari") {
   pari = true;
 } else if (userChoice === "dispari") {
   pari = false;
+} else {
+  alert("Errore! La pagina si ricaricherà e inizierai da capo.");
+  location.reload();
 }
 
-console.log(userChoice);
+console.log("YOU have chosen: " + userChoice);
 console.log(pari);
+
+// CPU choice
+
+let cpuNumber = autoRoll();
+console.log("CPU has chosen: " + cpuNumber);
 
 let userNumber = parseInt(prompt("Scegli un numero compreso tra 1 e 5."));
 
-const numList = ["1", "2", "3", "4", "5"];
+//Check number range
 
-if (numList.includes(userNumber, 0)) {
-  console.log("Hai fatto la tua scelta, ora sceglie il pc.");
-} else {
-  alert("Reinserisci il numero corretto!");
+if (userNumber > 5 || userNumber < 1) {
+  alert(
+    "Non hai inserito un numero da 1 a 5. La pagina si ricaricherà e inizierai da capo."
+  );
+  location.reload();
+} else if (isNaN(userNumber)) {
+  alert("Errore! La pagina si ricaricherà e inizierai da capo.");
+  location.reload();
 }
+
+console.log("YOU have chosen: " + userNumber);
